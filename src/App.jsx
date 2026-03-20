54d9f4ff48962bfeebf35dc0449b3ec7d9996dea
diodoarrahman <diodoarrahman2@gmail.com>
2026-03-20 21:06:20 +0700

    initial commit: setup taxvoice project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white text-2xl p-8">
+      TaxVoice berjalan! Tailwind v4 aktif ✅
+    </div>
+  )
+}
+
+export default App
